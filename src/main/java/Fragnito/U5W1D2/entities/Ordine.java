c7d696ae07d026840363af69d8ade232bc68cbd9
C043 <mariofragnitoph@gmail.com>
2024-09-03 14:49:55 +0200

    Update Ordine.java

--- src/main/java/Fragnito/U5W1D2/entities/Ordine.java
+++ src/main/java/Fragnito/U5W1D2/entities/Ordine.java
@@ -4,6 +4,7 @@ import Fragnito.U5W1D2.enums.StatoOrdine;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Calendar;
 import java.util.List;
@@ -20,12 +21,20 @@ public class Ordine {
     private int oraAcquisizione;
     private double totaleOrdine;
 
-    public Ordine(Tavolo tavolo, int numeroOrdine, List<Cibo> listaCibo, int numeroCoperti) {
+    public Ordine(Tavolo tavolo, int numeroOrdine, List<Cibo> listaCibo, int numeroCoperti, double coperto) {
         this.tavolo = tavolo;
         this.numeroOrdine = numeroOrdine;
         this.statoOrdine = StatoOrdine.IN_CORSO;
         this.listaCibo = listaCibo;
         this.numeroCoperti = numeroCoperti;
         this.oraAcquisizione = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        this.totaleOrdine = calcoloTotaleOrdine(coperto);
+    }
+
+    public double calcoloTotaleOrdine(double coperto){
+        return this.listaCibo
+                .stream()
+                .map(Cibo::getPrezzo)
+                .reduce(0.00, Double::sum) + this.numeroCoperti * coperto;
     }
 }
